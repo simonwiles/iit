54a27bc50560a0903812894c79ec8b71be363462
Simon Wiles <simonjwiles@gmail.com>
2019-10-24 13:55:42 -0700

    hard-code POST response from `agile/iit/twoviews`
    why on earth would you POST to a php backend to interpolate two local variables into an HTML template string?

--- js/comparison_viewer.js
+++ js/comparison_viewer.js
@@ -3,6 +3,60 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+// ! not es5 compatible!!
+var twoviews = `
+<div id = twoviews_container>
+    <p><span>Origin (click to reset):</span>
+        <input type="button" class="info_pane iit-button" id="image1_info" value="X:0 Y:0"/>
+        <input type="button" class="info_pane iit-button" id="image2_info" value="X:0 Y:0"/>
+        <input type="button" class="iit-button" name="ol_help" id="ol_help" value="Help"/>
+        <input type="button" class="iit-button" name="ol_close" id="ol_close" value = "Close"/>
+
+    </p>
+    <div id = "ol_i1" class = 'ol_image'>
+        <a href = "http://janbrueghel.net/sites/default/files/styles/iit-1200/public/objects/Adoration_of_the_Magi_%28Antwerp%2C_Mayer-van_den_Bergh%29.jpg?itok=Oto6UZEU" class = "zoom two_up">
+            <img class="two_up_image" src = 'http://janbrueghel.net/sites/default/files/styles/iit-1200/public/objects/Adoration_of_the_Magi_%28Antwerp%2C_Mayer-van_den_Bergh%29.jpg?itok=Oto6UZEU' style = 'float: left;'/>
+                <div class="crosshair-vertical"/>
+                <div class="crosshair-horizontal"/>
+        </a>
+    </div>
+    <div id = "ol_i2" class = 'ol_image'>
+        <a href = 'http://janbrueghel.net/sites/default/files/styles/iit-1200/public/objects/Adoration_of_the_Magi_%28Vienna%29.jpg?itok=OFkH4U7K' class = "zoom two_up">
+            <img  class="two_up_image" src = 'http://janbrueghel.net/sites/default/files/styles/iit-1200/public/objects/Adoration_of_the_Magi_%28Vienna%29.jpg?itok=OFkH4U7K' style = 'float: left;'/>
+            <div class="crosshair-vertical"/>
+            <div class="crosshair-horizontal"/>
+        </a>
+    </div>
+</div>
+`;
+
+var v = `
+<div id = twoviews_container>
+    <p><span>Origin (click to reset):</span>
+        <input type="button" class="info_pane iit-button" id="image1_info" value="X:0 Y:0"/>
+        <input type="button" class="info_pane iit-button" id="image2_info" value="X:0 Y:0"/>
+        <input type="button" class="iit-button" name="ol_help" id="ol_help" value="Help"/>
+        <input type="button" class="iit-button" name="ol_close" id="ol_close" value = "Close"/>
+
+    </p>
+    <div id = "ol_i1" class = 'ol_image'>
+        <a href = "<?php echo $img1_src; ?>" class = "zoom two_up">
+            <img class="two_up_image" src = '<?php echo $img1_src ?>' style = 'float: left;'/>
+                <div class="crosshair-vertical"/>
+                <div class="crosshair-horizontal"/>
+        </a>
+    </div>
+    <div id = "ol_i2" class = 'ol_image'>
+        <a href = '<?php echo $img2_src; ?>' class = "zoom two_up">
+            <img  class="two_up_image" src = '<?php echo $img2_src; ?>' style = 'float: left;'/>
+            <div class="crosshair-vertical"/>
+            <div class="crosshair-horizontal"/>
+        </a>
+    </div>
+</div>
+`;
+
 var image1Top, image2Top;
 var image1Left = 0;
 function comp_view() {
@@ -32,52 +86,53 @@ function comp_view() {
   var values = [];
   values.push({ name: "node1", value: node1 });
   values.push({ name: "node2", value: node2 });
-  $.post("agile/iit/twoviews", values, function(data) {
-    $("#overlay2").append(data);
-    $(window).scrollTop(0);
-    var ol_width = $("#overlay2").width();
-    var ol_height = $("#overlay2").height();
-    // Set image dimensions before calling zoomy.
-    var height = ol_height.toString() + "px";
-    $("#ol_i1").css("width", ol_width / 2 - 50);
-    $("#ol_i2").css("width", ol_width / 2 - 50); // note: this is called during window resize.
+  //  $.post("agile/iit/twoviews", values, function(data) {
+  var data = twoviews;
+  $("#overlay2").append(data);
+  $(window).scrollTop(0);
+  var ol_width = $("#overlay2").width();
+  var ol_height = $("#overlay2").height();
+  // Set image dimensions before calling zoomy.
+  var height = ol_height.toString() + "px";
+  $("#ol_i1").css("width", ol_width / 2 - 50);
+  $("#ol_i2").css("width", ol_width / 2 - 50); // note: this is called during window resize.
 
-    var top = Math.max($("#ol_i1").height(), $("#ol_i2").height()) + 5;
-    image1Top = top;
-    image2Top = top;
-    var src1 = $("#ol_i1")
-      .find("img")
-      .attr("src");
-    var scr2 = $("#ol_i1")
-      .find("img")
-      .attr("src");
-    var container_width = $("#ol_i1").width();
-    $(".zoom").zoomy({
-      zoomSize: container_width / 2,
-      round: false,
-      border: "6px solid #fff"
-    });
-    $.ajax({
-      url: "agile/iit/image_dimensions",
-      type: "POST",
-      data: {
-        img1: $("#ol_i1")
-          .find("img")
-          .attr("src"),
-        img2: $("#ol_i2")
-          .find("img")
-          .attr("src")
-      },
-      async: false,
-      success: function(results, status, xhr) {
-        results = JSON.parse(results);
-        dims = results;
-      },
-      error: function(data, status, xhd) {
-        console.log("The function execute_callback has failed");
-      }
-    });
+  var top = Math.max($("#ol_i1").height(), $("#ol_i2").height()) + 5;
+  image1Top = top;
+  image2Top = top;
+  var src1 = $("#ol_i1")
+    .find("img")
+    .attr("src");
+  var scr2 = $("#ol_i1")
+    .find("img")
+    .attr("src");
+  var container_width = $("#ol_i1").width();
+  $(".zoom").zoomy({
+    zoomSize: container_width / 2,
+    round: false,
+    border: "6px solid #fff"
   });
+  $.ajax({
+    url: "agile/iit/image_dimensions",
+    type: "POST",
+    data: {
+      img1: $("#ol_i1")
+        .find("img")
+        .attr("src"),
+      img2: $("#ol_i2")
+        .find("img")
+        .attr("src")
+    },
+    async: false,
+    success: function(results, status, xhr) {
+      results = JSON.parse(results);
+      dims = results;
+    },
+    error: function(data, status, xhd) {
+      console.log("The function execute_callback has failed");
+    }
+  });
+  //  });
 }
 function close_compview() {
   var dims = [];
